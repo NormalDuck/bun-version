9fe6e7c91899fdd313f57e31908b698700fe1c44
NormalDuck <1juhuang2@duck.com>
2024-10-08 13:45:11 -0700

    try bun

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/FTC-Scouting-App",
-    assetPrefix: '/FTC-Scouting-App/',
+    basePath: "/bun-version",
+    assetPrefix: '/bun-version/',
     output: "export",
     //strict mode rerenders twice, which slows down the performance.
     // reactStrictMode: true,
